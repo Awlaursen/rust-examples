c3dac140c7348cd1686f4a0ca953a39df19193cc
Albert Laursen <albert@bredenberg.dk>
2024-02-07 21:24:36 +0100

    functions basics

---
+++ 02-functions/src/main.rs
@@ -0,0 +1,31 @@
+
+// Function with no return value
+fn print_result<T: std::fmt::Display>(result: T) {
+    println!("result is: {}", result);
+}
+
+// Function with return value
+fn add_numbers(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+// Function with multiple return values
+fn add_and_multiply(x: i32, y: i32) -> (i32, i32) {
+    (x + y, x * y)
+}
+
+
+fn main() {
+    let x = 5;
+    let y = 2;
+
+    let result = add_numbers(x, y);
+    print_result(result);
+
+    // What happens if x and y are i64?
+    // let z: i64 = 5;
+    // let w: i64 = 2;
+
+    // let result = add_numbers(z, w);
+    // println!("result = {}", result);
+}
